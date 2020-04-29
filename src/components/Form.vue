ea173021f7af35a882c4f4ca65f03c526347285b
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-29 16:05:32 +0900

    formをcardに入れる

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,68 +1,70 @@
 <template>
   <div>
-    <b-form @submit="insert">
+    <b-card>
+        <b-form @submit="insert">
 
-      <b-form-group class="left" id="input-group-1" label="所属チーム:" label-for="input-1">
-        <b-form-select
-          id="input-1"
-          v-model="form.team"
-          :options="teams"
-          required
-        ></b-form-select>
-      </b-form-group>
+        <b-form-group class="left" id="input-group-1" label="所属チーム:" label-for="input-1">
+            <b-form-select
+            id="input-1"
+            v-model="form.team"
+            :options="teams"
+            required
+            ></b-form-select>
+        </b-form-group>
 
-      <b-form-group class="left" id="input-group-2" label="氏名:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.name"
-          required
-          placeholder="例. 山田"
-        ></b-form-input>
-      </b-form-group>
-      
-      <b-form-group class="left" id="input-group-3" label="strength1:" label-for="input-3">
-        <b-form-input
-          id="input-3"
-          v-model="form.strength1"
-          required
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group class="left" id="input-group-2" label="氏名:" label-for="input-2">
+            <b-form-input
+            id="input-2"
+            v-model="form.name"
+            required
+            placeholder="例. 山田"
+            ></b-form-input>
+        </b-form-group>
+        
+        <b-form-group class="left" id="input-group-3" label="strength1:" label-for="input-3">
+            <b-form-input
+            id="input-3"
+            v-model="form.strength1"
+            required
+            ></b-form-input>
+        </b-form-group>
 
-      <b-form-group class="left" id="input-group-4" label="strength2:" label-for="input-4">
-        <b-form-input
-          id="input-4"
-          v-model="form.strength2"
-          required
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group class="left" id="input-group-4" label="strength2:" label-for="input-4">
+            <b-form-input
+            id="input-4"
+            v-model="form.strength2"
+            required
+            ></b-form-input>
+        </b-form-group>
 
-      <b-form-group class="left" id="input-group-5" label="strength3:" label-for="input-5">
-        <b-form-input
-          id="input-5"
-          v-model="form.strength3"
-          required
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group class="left" id="input-group-5" label="strength3:" label-for="input-5">
+            <b-form-input
+            id="input-5"
+            v-model="form.strength3"
+            required
+            ></b-form-input>
+        </b-form-group>
 
-      <b-form-group class="left" id="input-group-6" label="strength4:" label-for="input-6">
-        <b-form-input
-          id="input-6"
-          v-model="form.strength4"
-          required
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group class="left" id="input-group-6" label="strength4:" label-for="input-6">
+            <b-form-input
+            id="input-6"
+            v-model="form.strength4"
+            required
+            ></b-form-input>
+        </b-form-group>
 
-      <b-form-group class="left" id="input-group-7" label="strength5:" label-for="input-7">
-        <b-form-input
-          id="input-7"
-          v-model="form.strength5"
-          required
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group class="left" id="input-group-7" label="strength5:" label-for="input-7">
+            <b-form-input
+            id="input-7"
+            v-model="form.strength5"
+            required
+            ></b-form-input>
+        </b-form-group>
 
 
-      <b-button type="submit">登録</b-button>
-    </b-form>
+        <b-button type="submit">登録</b-button>
+        </b-form>
+    </b-card>
   </div>
 </template>
 
