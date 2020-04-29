43e3cc948985dc9f5d2536adb27a97c99377781c
Masaya Nakayama <uw.msy.0913@gmail.com>
2020-04-29 15:21:54 +0900

    検索機能の実装

---
+++ src/views/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="search">
+    <b-container>
+      <SearchItem/>
+    </b-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SearchItem from '@/components/SearchItem.vue'
+
+export default {
+  name: 'Search',
+  components: {
+    SearchItem
+  }
+}
+</script>
